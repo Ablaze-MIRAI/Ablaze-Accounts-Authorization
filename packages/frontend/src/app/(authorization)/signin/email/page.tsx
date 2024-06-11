0973a65cba8c94c367895ac6f14502504687d271
Raisan <yuri@rainyan.net>
2024-06-11 23:05:27 +0900

    [Add] return button

--- packages/frontend/src/app/(authorization)/signin/email/page.tsx
+++ packages/frontend/src/app/(authorization)/signin/email/page.tsx
@@ -1,11 +1,20 @@
+// UI
+import { Button } from "@/components/ui/button";
+
 // Utility
 import { SignContainer } from "@/components/layouts/SignContainer";
 import { SigninEmailVerifyForm } from "@/components/parts/SigninEmailForm";
+import Link from "next/link";
 
 export default function SigninEmail(){
   return (
     <SignContainer title="アカウントにログイン - Eメール">
-      <SigninEmailVerifyForm/>
+      <div className="space-y-2">
+        <SigninEmailVerifyForm/>
+        <Button variant="secondary" className="w-full" asChild>
+          <Link href="/signin">戻る</Link>
+        </Button>
+      </div>
     </SignContainer>
   );
 }
