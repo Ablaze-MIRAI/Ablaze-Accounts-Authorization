b30e710222edfeaa22a5caa8130b3c5ff7766897
Raisan <yuri@rainyan.net>
2024-06-11 11:55:39 +0000

    [Add] Signin Page

---
+++ packages/frontend/src/app/(authorization)/signin/page.tsx
@@ -0,0 +1,23 @@
+// React/Next
+import Link from "next/link";
+
+// UI
+import { Button } from "@/components/ui/button";
+
+// Utility
+import { SignContainer } from "@/components/layouts/SignContainer";
+import { SignMethodLinksContainer, SignMethodLinks, SignSeparate } from "@/components/layouts/SignMethods";
+
+export default function SigninRoot(){
+  return (
+    <SignContainer title="アカウントにログイン">
+      <SignMethodLinksContainer>
+        <SignMethodLinks type="signin"/>
+        <SignSeparate/>
+        <Button variant="secondary" asChild>
+          <Link href="/signup">登録する</Link>
+        </Button>
+      </SignMethodLinksContainer>
+    </SignContainer>
+  );
+}
