9f6b1d1c8cc0a1cef9a5005bbeb1a8c1d70dea02
Raisan <yuri@rainyan.net>
2024-06-20 08:24:17 +0000

    Pending changes exported from your codespace

--- packages/backend/src/env.ts
+++ packages/backend/src/env.ts
@@ -4,7 +4,7 @@ export default {
   // SMTP Server
   SMTP_HOST: process.env.SMTP_HOST ?? "localhost",
   SMTP_PORT: Number(process.env.SMTP_PORT ?? 587),
-  SMTP_SECURE: process.env.SMTP_SECURE === "secure",
+  SMTP_SECURE: process.env.SMTP_SECURE === "yes",
   SMTP_USER: process.env.SMTP_USER ?? "root",
   SMTP_PASS: process.env.SMTP_PASS ?? "root",
   SMTP_SENDER_ADDR: process.env.SMTP_SENDER_ADDR ?? "noreply@localhost",
