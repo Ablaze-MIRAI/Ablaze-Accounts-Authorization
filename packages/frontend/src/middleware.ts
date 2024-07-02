fff68913f5d0a3a94ddd237c784ce3423052535d
Raisan <yuri@rainyan.net>
2024-07-02 09:54:15 +0000

    Pending changes exported from your codespace

--- packages/frontend/src/middleware.ts
+++ packages/frontend/src/middleware.ts
@@ -4,20 +4,14 @@ import { parse as cookieParser } from "set-cookie-parser";
 export const middleware = async (request: NextRequest) =>{
   const response = NextResponse.next();
 
-  console.log("[M1]", request.nextUrl.href);
+  console.log("[MIDDLEWARE RUNNING]");
 
 
   const backendsession = request.cookies.get("backendsession")?.value;
   if(backendsession) return;
 
-  console.log("[M2]", backendsession);
-
-  console.log(backendsession);
-
   const hukkatunojyumon = request.cookies.get("hukkatunojyumon")?.value;
   if(!hukkatunojyumon) return;
-
-  console.log(hukkatunojyumon);
 
   const resp = await fetch("http://localhost:3000/api/user/revival", {
     method: "POST",
@@ -28,24 +22,14 @@ export const middleware = async (request: NextRequest) =>{
   });
 
   const revival = await resp.json();
-  console.log(revival)
   if(!revival.success) return;
 
+  console.log(resp.headers)
+
   const setcookie = resp.headers.getSetCookie();
-  const cookie = cookieParser(setcookie);
-  //@ts-ignore
-  console.log("[PARSED COOKIE]", cookie[0]);
 
-  const backendsession_cookie = cookie.find(v => v.name === "backendsession");
-  console.log("[M3b]", backendsession_cookie);
-  if(!backendsession_cookie) return;
-  console.log("[M3]", backendsession_cookie.value)
-  response.cookies.set({
-    name: "backendsession",
-    value: backendsession_cookie.value,
-    path: "/"
-  })
-
+  //response.headers.set("Set-Cookie")
+  console.log("[MIDDLEWARE RESPONSE]");
   return response;
 }
 
