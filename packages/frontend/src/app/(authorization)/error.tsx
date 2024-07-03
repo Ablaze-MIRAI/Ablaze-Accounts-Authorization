0b0294c3fdde8e8d49b73906be7197e83a3cb2b4
Raisan <yuri@rainyan.net>
2024-07-03 04:01:07 +0000

    無駄なスペースを削除

--- packages/frontend/src/app/(authorization)/error.tsx
+++ packages/frontend/src/app/(authorization)/error.tsx
@@ -5,7 +5,7 @@ import { Button } from "@/components/ui/button";
 export default function Error({ error, reset }: { error: Error, reset: () => void }){
   return (
     <div>
-      <h2>{ error.message }</h2>
+      <h2>{error.message}</h2>
       <Button variant="default" onClick={() => reset()}>再レンダリングする</Button>
     </div>
   )
