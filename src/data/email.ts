1ae29aaef8a281c96bda0aedb5f0eed28e3de50d
Raisan <yuri@rainyan.net>
2024-07-12 19:48:35 +0900

    Data store helper

---
+++ src/data/email.ts
@@ -0,0 +1,38 @@
+import { prisma } from "@/library/prisma";
+
+export const checkExistsUser = async (email: string): Promise<boolean> =>{
+  const isExist = !!await prisma.idpEmail.findUnique({
+    select: { id: true },
+    where: { email: email }
+  });
+
+  return isExist;
+};
+
+export const createUserWithEmail = async (email: string, hashed_password: string) =>{
+  const name = email.split("@")[0];
+  const avatar = `https://source.boringavatars.com/beam/128/${name}?square`;
+
+  return await prisma.user.create({
+    data: {
+      screen_name: name,
+      avatar: avatar,
+      idp_email: {
+        create: {
+          email: email,
+          password: hashed_password
+        }
+      }
+    }
+  });
+};
+
+export const getUserWithEmail = async (email: string) =>{
+  return await prisma.idpEmail.findUnique({
+    where: { email: email },
+    select: {
+      password: true,
+      uid: true
+    }
+  });
+};
---
+++ src/data/session.ts
@@ -0,0 +1,42 @@
+import { prisma } from "@/library/prisma";
+import { redis } from "@/library/redis";
+import type { UserSession } from "@/typings/session";
+
+const session_store_prefix = "_session";
+const session_expires = 60*60*2;
+
+export const getUserSession = async (key: string): Promise<UserSession | undefined> =>{
+  const user = await redis.hgetall(`${session_store_prefix}_${key}`);
+  if(Object.keys(user).length === 0) return undefined;
+  await redis.expire(`${session_store_prefix}_${key}`, session_expires);
+
+  return user as UserSession;
+};
+
+export const getRestoreToken = async (restore_token: string) =>{
+  return await prisma.restoreToken.findUnique({
+    where: { token: restore_token },
+    select: {
+      updatedAt: true,
+      uid: true,
+      user: {
+        select: {
+          screen_name: true,
+          avatar: true,
+          account_type: true
+        }
+      }
+    }
+  });
+};
+
+export const updateRestoreToken = async (token: string, newtoken: string) =>{
+  try{
+    return await prisma.restoreToken.update({
+      where: { token: token },
+      data: { token: newtoken }
+    });
+  }catch(e){
+    return undefined;
+  }
+};
