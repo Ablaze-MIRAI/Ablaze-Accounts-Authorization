2f1b558a7fe465600a3d1f6c2592be7f9bee871d
Raisan <yuri@rainyan.net>
2024-07-16 01:12:59 +0900

    パスワードバリデーション コメントを追加

--- src/app/(authorization)/(unsign-only)/signup/email/schema.ts
+++ src/app/(authorization)/(unsign-only)/signup/email/schema.ts
@@ -2,6 +2,7 @@ import { z } from "zod";
 
 export const EmailSignupSchema = z.object({
   email: z.string().min(6).max(255).email(),
+  // 記号,半角英数を許可。大文字小文字数字必須のパスワード
   password: z.string().min(8).max(48).regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z0-9!-/:-@[-`{-~]+$/),
   retype: z.string().min(8).max(48)
 }).superRefine(({ password, retype }, ctx) =>{
