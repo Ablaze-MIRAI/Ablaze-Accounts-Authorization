ff6ee522812ca3abb382a75ec36aedf66ec56e5d
Raisan <yuri@rainyan.net>
2024-07-15 19:26:41 +0900

    Database migration

--- prisma/migrations/20240715102557_init/migration.sql
+++ prisma/migrations/20240715102557_init/migration.sql
@@ -1,6 +1,9 @@
 -- CreateEnum
 CREATE TYPE "AccountType" AS ENUM ('GENERAL', 'MODERATOR', 'SUPERUSER');
 
+-- CreateEnum
+CREATE TYPE "ClientType" AS ENUM ('PUBLIC', 'CONFIDENTIAL');
+
 -- CreateTable
 CREATE TABLE "User" (
     "uid" TEXT NOT NULL,
@@ -33,6 +36,31 @@ CREATE TABLE "IdpEmail" (
     CONSTRAINT "IdpEmail_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "AcceptApp" (
+    "id" TEXT NOT NULL,
+    "client_id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "uid" TEXT NOT NULL,
+
+    CONSTRAINT "AcceptApp_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "RefreshToken" (
+    "id" TEXT NOT NULL,
+    "token" TEXT NOT NULL,
+    "expiration" TIMESTAMP(3) NOT NULL,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "client_id" TEXT NOT NULL,
+    "client_type" "ClientType" NOT NULL,
+    "scope" TEXT NOT NULL,
+    "uid" TEXT NOT NULL,
+
+    CONSTRAINT "RefreshToken_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "RestoreToken_token_key" ON "RestoreToken"("token");
 
@@ -42,8 +70,17 @@ CREATE UNIQUE INDEX "IdpEmail_email_key" ON "IdpEmail"("email");
 -- CreateIndex
 CREATE UNIQUE INDEX "IdpEmail_uid_key" ON "IdpEmail"("uid");
 
+-- CreateIndex
+CREATE UNIQUE INDEX "RefreshToken_token_key" ON "RefreshToken"("token");
+
 -- AddForeignKey
 ALTER TABLE "RestoreToken" ADD CONSTRAINT "RestoreToken_uid_fkey" FOREIGN KEY ("uid") REFERENCES "User"("uid") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "IdpEmail" ADD CONSTRAINT "IdpEmail_uid_fkey" FOREIGN KEY ("uid") REFERENCES "User"("uid") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "AcceptApp" ADD CONSTRAINT "AcceptApp_uid_fkey" FOREIGN KEY ("uid") REFERENCES "User"("uid") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "RefreshToken" ADD CONSTRAINT "RefreshToken_uid_fkey" FOREIGN KEY ("uid") REFERENCES "User"("uid") ON DELETE CASCADE ON UPDATE CASCADE;
