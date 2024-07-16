ed8cf8cd710a97af8861261df707c8e0d6088cca
Raisan <yuri@rainyan.net>
2024-07-16 12:19:00 +0900

    [Fix] #18 通常遷移してしまう問題を修正

--- src/components/containers/AuthorizationContainer.tsx
+++ src/components/containers/AuthorizationContainer.tsx
@@ -1,5 +1,6 @@
 import type { ReactNode } from "react";
 import Image from "next/image";
+import Link from "next/link";
 import AblazeLogoBlack from "@/assets/logo/black.svg";
 
 export const AuthorizationContainer = ({ children }: Readonly<{ children: ReactNode }>) =>{
@@ -7,9 +8,9 @@ export const AuthorizationContainer = ({ children }: Readonly<{ children: ReactN
     <>
       <header className="fixed top-0 left-0 z-30 w-full h-[10svh] px-4 sm:px-16 flex justify-between items-center">
         <div className="h-1/2">
-          <a href="/" title="Ablaze Accounts">
-            <Image src={AblazeLogoBlack} width={0} height={0} alt="Ablaze Logo" className="h-full w-auto" priority={true}/>
-          </a>
+          <Link href="/">
+            <Image src={AblazeLogoBlack} width={100} height={200} alt="Ablaze Logo" className="h-full w-auto" priority={true}/>
+          </Link>
         </div>
       </header>
       <div className="h-[100svh] flex justify-center items-center">
