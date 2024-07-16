61ffff344314cef86721390b9f75996d1f6e8779
Raisan <yuri@rainyan.net>
2024-07-16 12:31:13 +0900

    パスワードバリデーションに日本語メッセージを追加

--- src/app/(authorization)/(unsign-only)/signup/email/schema.ts
+++ src/app/(authorization)/(unsign-only)/signup/email/schema.ts
@@ -3,8 +3,8 @@ import { z } from "zod";
 export const EmailSignupSchema = z.object({
   email: z.string().min(6).max(255).email(),
   // 記号,半角英数を許可。大文字小文字数字必須のパスワード
-  password: z.string().min(8).max(48).regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z0-9!-/:-@[-`{-~]+$/),
-  retype: z.string().min(8).max(48)
+  password: z.string().min(8, "8文字以上で設定してください").max(48, "48文字以下で設定してください").regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z0-9!-/:-@[-`{-~]+$/, "大文字小文字数字を含む半角英数記号にしてください"),
+  retype: z.string()
 }).superRefine(({ password, retype }, ctx) =>{
   if(password !== retype) ctx.addIssue({
     path: ["retype"],
