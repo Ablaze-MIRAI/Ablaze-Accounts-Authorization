177e4fe1f634061c374a3a0460152b6aba3842f0
Raisan <yuri@rainyan.net>
2024-08-13 22:01:19 +0900

    evacuate

--- src/app/(authorization)/(unsign-only)/auth/google/route.tsx
+++ src/app/(authorization)/(unsign-only)/auth/google/route.tsx
@@ -6,13 +6,17 @@ import { randomBytes } from "crypto";
 import { createGoogleOAuth2Client } from "@/library/connection/googleapis";
 
 export async function GET(){
+  const state = randomBytes(32).toString("hex");
   const client = createGoogleOAuth2Client();
 
-  const url = client.generateAuthUrl
+  const url = client.generateAuthUrl({
+    scope: ["https://www.googleapis.com/auth/userinfo.email"],
+    state: state
+  });
 
-  const state = randomBytes(32).toString("hex");
+  cookies().set(environment.COOKIE_OAUTH2_STATE, state, {
+    maxAge: 60*10
+  });
 
-  cookies().set(environment.COOKIE_OAUTH2_STATE, state);
-
-  redirect("");
+  redirect(url);
 }
