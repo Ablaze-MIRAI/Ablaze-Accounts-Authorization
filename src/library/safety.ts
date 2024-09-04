f85a36da092c5c565d5c660926b02863f261d7ba
Raisan <yuri@rainyan.net>
2024-09-05 04:54:56 +0900

    #57

--- src/library/safety.ts
+++ src/library/safety.ts
@@ -1,4 +1,12 @@
+import { createHash } from "crypto";
+
 export const SafetyURI = (address: string) =>{
   if(address.startsWith("http")) return "/";
   return address;
+};
+
+export const TokenHash = (token: string) =>{
+  const hash = createHash("sha256");
+  hash.update(token);
+  return hash.digest("hex");
 };
