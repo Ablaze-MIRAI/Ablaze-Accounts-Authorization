2ed9bff026753554b7ac16603281bb05fb37a519
Raisan <yuri@rainyan.net>
2024-09-05 04:55:07 +0900

    #57

--- src/constants/general.ts
+++ src/constants/general.ts
@@ -1,2 +1,8 @@
 export const general = {
+  APP_NAME: "Ablaze Accounts",
+  APP_SUBTITLE: "すべてのAblazeアプリをこれひとつで",
+  APP_COMMON_TITLE: "Ablaze Accounts - すべてのAblazeサービスをこれひとつで",
+  APP_SHORT_DESCRIPTION: "すべてのAblazeサービスが利用できるアカウントサービスです",
+  APP_URL: "https://accounts.ablaze.one",
+  OGP_IMAGE_URI: "https://cdn.ablaze.one/ogp/ablaze-accounts-new.png"
 };
