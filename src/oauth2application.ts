3475e9819e313e470aefd0642204e6de144fad1a
Raisan <yuri@rainyan.net>
2024-10-16 22:37:17 +0900

    [Edit] コンフィグを編集

--- src/oauth2application.ts
+++ src/oauth2application.ts
@@ -30,7 +30,7 @@ const applications: ApplicationsType = {
     client: "PUBLIC",
     scope: "user",
     callback: ["https://forum.mirairo.dev/oauth2/callback", "https://forum.ablaze.one/oauth2/callback", "http://localhost:3000/oauth2/callback", "http://localhost:3001/oauth2/callback"],
-    //logout: "https://forum.ablaze.one/oidc/logout"
+    logout: "https://forum.ablaze.one/oidc/logout"
   }
 };
 
--- src/typings/session.ts
+++ src/typings/session.ts
@@ -1,7 +1,7 @@
 import type { $Enums } from "@prisma/client";
 
 export type UserSession = {
-  id: string // Restore ID
+  id: string, // Restore ID
   uid: string,
   name: string,
   avatar: string,
